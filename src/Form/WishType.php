774a5c0f5992c5712455f028092f63c724525524
HnHoussi <hanoun.houssin@gmail.com>
2025-08-19 14:54:27 +0200

    add form validation

---
+++ src/Form/WishType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Wish;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WishType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Title',
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('author')
+            ->add('submit', SubmitType::class, [
+                'label' => 'Submit',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Wish::class,
+        ]);
+    }
+}
